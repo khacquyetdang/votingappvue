361c6bf59bee1001b49f0ec1895bd745d8747328
mrdang <khacquyet.dang.pro@gmail.com>
2018-04-15 23:37:54 +0200

    add register, add prettier  extension for better autoformat

---
+++ client/src/components/Register.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" v-model="email" placeholder="Email">
+    <br>
+    <input type="password" name="password" v-model="password" placeholder="password">
+    <br>
+    <button>Register</button>
+    <br>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
