b320fa8206656fcf84502b6bf432dd5cdb6e23da
mrdang <khacquyet.dang.pro@gmail.com>
2018-04-17 23:36:53 +0200

    register page ok, prettier config for backend ok

--- src/logger.js
+++ src/logger.js
@@ -10,9 +10,8 @@ const divider = chalk.gray('\n-----------------------------------');
  * Logger middleware, you can customize it to make messages more personal
  */
 const logger = {
-
   // Called whenever there's an error on the server we want to print
-  error: (err) => {
+  error: err => {
     console.error(chalk.red(err));
   },
 
@@ -28,12 +27,13 @@ const logger = {
     console.log(`
 ${chalk.bold('Access URLs:')}${divider}
 Localhost: ${chalk.magenta(`http://${host}:${port}`)}
-      LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) + (tunnelStarted
-    ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}`
-    : '')}${divider}
+      LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) +
+        (tunnelStarted
+          ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}`
+          : '')}${divider}
 ${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
     `);
-  }
+  },
 };
 
 module.exports = logger;
