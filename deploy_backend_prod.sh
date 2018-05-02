5cb344f48aa89a4e7e5842232a14b69b64f84f1e
mrdang <khacquyet.dang.pro@gmail.com>
2018-05-02 08:03:23 +0200

    add doc and build pack

--- deploy_backend_prod.sh
+++ deploy_backend_prod.sh
@@ -1,8 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+# to delete an app heroku apps:destroy --app votingappvue
+APPS_HEROKU=$(heroku apps);
+#echo $APPS_HEROKU;
+WEB=votingappvue
+API=votingappvueapi
+if [[ "${APPS_HEROKU}" != *$WEB* ]];
+then
+  echo "create $WEB"
+  heroku create $WEB --remote $WEB
+fi
 
+if [[ " ${APPS_HEROKU} " !=  *$API* ]];
+then
+  echo "create  $API"
+  heroku create $API --remote $API
+fi
+
+echo "end";
+
+: '
 heroku create votingappvueapi --remote votingappvueapi
 heroku create votingappvue --remote votingappvue
-git push votingappvueapi master
-git push votingappvue master
-heroku ps:scale web=1 --remote webserver-app-name
-heroku ps:scale api=1 --remote apiserver-app-name
+git push heroku master
+heroku ps:scale web=1 --remote votingappvue
+heroku ps:scale api=1 --remote votingappvueapi
+'
