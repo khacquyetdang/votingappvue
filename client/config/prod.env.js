4034ce5fb31c32e6de1fc8fb45b5360408c8bcf1
mrdang <khacquyet.dang.pro@gmail.com>
2018-05-04 08:36:33 +0200

    [frontend] prod config load env

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,4 +1,11 @@
-'use strict'
+'use strict';
+
+const merge = require('webpack-merge');
+const dotenv = require('dotenv');
+
+const resultDotenv = dotenv.load({ path: '.env.production' });
+
 module.exports = {
-  NODE_ENV: '"production"'
-}
+  NODE_ENV: '"production"',
+  baseURL: JSON.stringify(process.env.baseURL),
+};
