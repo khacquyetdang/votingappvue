c59475ccd35a59bf803e0567f349c93364a5f0d0
mrdang <khacquyet.dang.pro@gmail.com>
2018-05-04 08:51:41 +0200

    [frontend] in progress, build prod

--- client/config/index.js
+++ client/config/index.js
@@ -44,12 +44,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../dist/index.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/dist',
 
     /**
      * Source Maps
