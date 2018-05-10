ed5ae7535546f638e1555f68c16c38c563afdb10
mrdang <khacquyet.dang.pro@gmail.com>
2018-05-10 10:04:11 +0200

    in progress, config cors for client

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,4 +1,6 @@
-var whitelist = ['http://127.0.0.1:3000/', 'http://192.168.0.10:3000']
+// normally move these params into env file
+var whitelist = ['http://127.0.0.1:3000/', 'http://192.168.0.10:3000', 
+'https://votingappvueapi.khacquyetdang.com', 'https://votingapp.khacquyetdang.com']
 exports.corsOptions = {
     origin: whitelist
 }
