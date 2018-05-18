473d84594c787d295f3dc860060e232ea07e4e1a
mrdang <khacquyet.dang.pro@gmail.com>
2018-05-18 09:09:25 +0200

    in progress, test unit with karma

--- client/test/unit/specs/HelloWorld.spec.js
+++ client/test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,14 @@
-import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+require('babel-polyfill'); 
+import Vue from 'vue';
+import AboutPage from '@/components/About';
 
-describe('HelloWorld.vue', () => {
+describe('AboutPage component', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App')
-  })
-})
+    const Constructor = Vue.extend(AboutPage);
+    const vm = new Constructor().$mount();
+   // console.log("vm el", vm.$el);
+    console.log("vm el h1 text content", vm.$el.querySelector('.home h1').textContent);
+    expect(vm.$el.querySelector('.home h1').textContent)
+      .toEqual('Welcome to Your Vue.js App');
+  });
+});
